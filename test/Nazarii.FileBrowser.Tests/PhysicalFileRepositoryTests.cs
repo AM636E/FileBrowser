415637aa17d02ad3692e49cc0438f28500d11961
Nazarii Brenchuk <bnazariy@gmail.com>
2016-02-12 12:51:46 +0200

    Added tests

---
+++ test/Nazarii.FileBrowser.Tests/PhysicalFileRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nazarii.FileBrowser.Repositories;
+using Xunit;
+
+namespace Nazarii.FileBrowser.Tests
+{
+    public class PhysicalFileRepositoryTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var repository = new PhysicalFileRepository();
+            var result = repository.GetFolderStatistics("./TestFolder");
+            Assert.Equal(8, result.SmallFiles);
+        }
+    }
+}
