57a747473990a1396f5934b3eed1bf88d143e8f9
Nazarii Brenchuk <bnazariy@gmail.com>
2016-02-12 16:42:31 +0200

    Added loading bar

--- src/Nazarii.FileBrowser/typescript/app.js
+++ src/Nazarii.FileBrowser/typescript/app.js
@@ -1,5 +1,5 @@
 (function (ng) {
-    ng.module("nazarii.filebrowser", ["ngRoute"])
+    ng.module("nazarii.filebrowser", ["ngRoute", "angular-loading-bar"])
         .controller("empty", function () { })
         .directive("nazariiFileBrowser", Nazarii.FileBrowser.Directives.fileBrowserDirective)
         .service("FileService", Nazarii.FileBrowser.Services.DefaultFileService);
--- src/Nazarii.FileBrowser/typescript/app.ts
+++ src/Nazarii.FileBrowser/typescript/app.ts
@@ -1,5 +1,5 @@
 ﻿(ng => {
-    ng.module("nazarii.filebrowser", ["ngRoute"])
+    ng.module("nazarii.filebrowser", ["ngRoute", "angular-loading-bar"])
         .controller("empty", () => { })
         .directive("nazariiFileBrowser", Nazarii.FileBrowser.Directives.fileBrowserDirective)
         .service("FileService", Nazarii.FileBrowser.Services.DefaultFileService);
