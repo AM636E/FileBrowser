6c32b9bec0a8103820ac218e6b915c000d40d6d2
Nazarii Brenchuk <bnazariy@gmail.com>
2016-02-12 13:12:14 +0200

    Added GetDirectoryInfo tests

--- test/Nazarii.FileBrowser.Tests/PhysicalFileRepositoryTests.cs
+++ test/Nazarii.FileBrowser.Tests/PhysicalFileRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Nazarii.FileBrowser.Repositories;
 using Xunit;
 
@@ -7,13 +8,29 @@ namespace Nazarii.FileBrowser.Tests
     public class PhysicalFileRepositoryTests
     {
         [Fact]
-        public void PassingTest()
+        public void GetFolderStatisticsTest()
         {
             var repository = new PhysicalFileRepository(2, 5, 10);
             var result = repository.GetFolderStatistics("./TestFolder");
             Assert.Equal(9, result.SmallFiles);
             Assert.Equal(4, result.BigFiles);
             Assert.Equal(6, result.MiddleFiles);
+        }
+
+        [Fact]
+        public void GetDirectoryInfoTest()
+        {
+            var repository = new PhysicalFileRepository(2, 5, 10);
+            var result = repository.GetDirectoryInfo("./TestFolder");
+            Assert.Equal(4, result.Children.Count());
+        }
+
+        [Fact]
+        public void GetDirectoryInfoTest1()
+        {
+            var repository = new PhysicalFileRepository(2, 5, 10);
+            var result = repository.GetDirectoryInfo("./TestFolder/Test");
+            Assert.Equal(8, result.Children.Count());
         }
     }
 }
